a34ff460b624a43b54699d090f9e5aaa63ad035a
Ankush Chhabra <ankushchhabra2002@gmail.com>
2025-04-24 15:57:27 +0530

    build

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,10 +7,12 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import { app, server } from "./lib/socket.js";
+import path from "path";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "50mb" })); // Parse JSON payloads with a size limit of 50MB
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
@@ -24,6 +26,14 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log("Server is running on Port: " + PORT);
